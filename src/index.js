06a32bdbbabc01469c434fdce9e14e3ba34c533f
virendergit1 <ch_virender@yahoo.com>
2017-02-12 23:11:03 -0600

    added firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {Provider} from 'react-redux';
+import * as firebase from 'firebase';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -11,8 +12,20 @@ import configureStore from './store/configureStore.dev';
 import {loadVisits} from './actions/visitFormActions';
 import {loadIncidents} from './actions/incidentFormActions';
 
+// for materil-ui
 injectTapEventPlugin();
 
+// Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyB7TxNNQbiTJKYUXSN62N89xaSob6Qx26U",
+    authDomain: "mysecurecomplex.firebaseapp.com",
+    databaseURL: "https://mysecurecomplex.firebaseio.com",
+    storageBucket: "mysecurecomplex.appspot.com",
+    messagingSenderId: "180974727277"
+  };
+  firebase.initializeApp(config);
+
+// configure store
 const store = configureStore();
 store.dispatch(loadVisits());
 store.dispatch(loadIncidents());
